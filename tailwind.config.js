34d5a2074ca52faf03085debdb3c6e8a826cf31c
kshyam <shyam@codecrux.com>
2026-02-09 20:07:22 +0530

    npm added

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_pages/**/*.html",
+    "./v3/**/*.html"
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        border: "hsl(240 3.7% 15.9%)",
+        input: "hsl(240 3.7% 15.9%)",
+        ring: "hsl(240 4.9% 83.9%)",
+        background: "hsl(240 10% 3.9%)",
+        foreground: "hsl(0 0% 98%)",
+        primary: {
+          DEFAULT: "hsl(0 0% 98%)",
+          foreground: "hsl(240 5.9% 10%)",
+        },
+        secondary: {
+          DEFAULT: "hsl(240 3.7% 15.9%)",
+          foreground: "hsl(0 0% 98%)",
+        },
+        muted: {
+          DEFAULT: "hsl(240 3.7% 15.9%)",
+          foreground: "hsl(240 5% 64.9%)",
+        },
+        accent: {
+          DEFAULT: "hsl(240 3.7% 15.9%)",
+          foreground: "hsl(0 0% 98%)",
+        },
+      },
+      borderRadius: {
+        lg: "0.5rem",
+        md: "calc(0.5rem - 2px)",
+        sm: "calc(0.5rem - 4px)",
+      },
+    }
+  },
+  plugins: [],
+}
